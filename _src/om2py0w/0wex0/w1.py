67aedd7e0f6aaf40627d37980e2a2aac744d0e94
unknown <>
2015-10-28 17:01:03 +0800

    1028@office

--- _src/om2py0w/0wex0/w1.py
+++ _src/om2py0w/0wex0/w1.py
@@ -42,37 +42,3 @@ if __name__  == '__main__':
     else:
         foo()
 
-"""
-diary = open('Diary.txt', 'a+')
-
-print "I will open the following file: %r" % diary.name
-
-text = ""
-stopword = "q"
-print "Enter \'q\' to quit input"
-now = date.todayf
-
-while True:
-	line = raw_input('input >')
-	if line.strip() == stopword:
-		break
-	text += "%s\n" % line
-	
-diary.write(text)
-
-＃将文件指针启示设置为0
-diary.seek(0)
-
-print_diary = "If you want to print diary, Enter \'y\'?"
-mark = raw_input(print_diary)
-
-
-if mark.lower() == 'y':
-	print "The content of the diary is:"
-	print diary.read()
-else:
-	print "quit the diary"
-		
-	
-diary.close()
-"""
---
+++ note/w2_task.md
@@ -0,0 +1,9 @@
+
+
+AttributeError: App instance has no attribute 'master'
+
+
+ Non-ASCII character 
+
+     Frame.__init__(self, master)
+TypeError: unbound method __init__() must be called with Frame instance as first argument (got App instance instead)
