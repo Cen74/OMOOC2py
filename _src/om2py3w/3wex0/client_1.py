dea6946f12e64537996ed9373d37d73667f3e498
lihongcen <lihongcen@janetwentekiMacBook-Pro.local>
2015-11-05 08:32:57 +0800

    debug

--- _src/om2py3w/3wex0/client_1.py
+++ _src/om2py3w/3wex0/client_1.py
@@ -13,11 +13,15 @@ ADDR = (HOST,PORT)
 diary_client = socket(AF_INET,SOCK_DGRAM) 
 
 input_c = raw_input(' > ')
-data = input_c.decode('UTF-8').encode('UTF-8')
+print "input_c: %r" % input_c, input_c
+data = input_c #.decode('UTF-8').encode('UTF-8')
+print "data: %r" % data, data
+
 
 diary_client.sendto(data, ADDR)
 (recv, addr) = diary_client.recvfrom(BUFSIZ)
 
-print "recv %r, from %r " % (recv.decode('UTF-8'), addr)
+print "recv %r, from %r " % (recv, addr)#.decode('UTF-8'), addr)
+print recv
 
 diary_client.close()
