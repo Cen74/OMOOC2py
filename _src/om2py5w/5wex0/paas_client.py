b3a18352e56eca38a126baa45b33fedd4807a80e
Cen74 <52993580@qq.com>
2015-11-16 23:13:59 +0800

    add 2 notes

---
+++ _src/om2py5w/5wex0/paas_client.py
@@ -0,0 +1,62 @@
+ # -*- coding: UTF-8 -*-  
+import requests
+import sys
+import os 
+
+
+'''
+def print_html(data):
+	content = BeautifulSoup(data, "html.parser")
+	for line in content.find_all('br'):
+		print line.get_text()
+'''
+
+def foo():
+
+
+	login = requests.get('http://localhost:8080/cmd')
+
+	print login.text
+	print '-'*10
+
+
+	TAG = '>>'
+
+	while True:
+		input_c = raw_input('%s>' % TAG)
+
+		if input_c in ['help', 'h', '?']:
+			print """
+			   \'q\' or \'quit\' \'bye\' for help
+ 
+			   \'h\' or \'help\' or \'bye\' to exit
+
+			   syn to return history
+
+			   st:TAG to set tags
+
+			   ls: list TAGs 
+
+			   FLUSH to clear diary
+
+			  """
+		elif input_c in ['bye', 'q' ,'quit']:
+			print "bye"
+			break
+		elif input_c == 'syn':
+			history = requests.get('http://localhost:8080/diary')
+			print "The diary histroy is :", '-'*10
+			print_html(history.text)
+
+		else:
+			requests.post("http://localhost:8080/cmd", data = {"key":input_c})
+
+
+
+if __name__  == '__main__':
+    if 1 != len(sys.argv) :
+        print ''' Usage: $python main.py '''
+    else:
+        foo()
+
+
---
+++ _src/om2py5w/5wex0/pass_ser.py
@@ -0,0 +1,80 @@
+
+from datetime import datetime
+import sys
+import os
+from bottle import Bottle, route, run, template, request, get, post, view
+
+import sae
+import sae.kvdb
+
+kv = sae.kvdb.Client()
+
+app = Bottle()
+
+@app.route('/')
+def hello():
+    return "Hello, world! - Bottle"
+
+
+def diary_lines():
+    content = [] 
+    for i in kv.getkeys_by_prefix('Null'):
+        content += [kv.get(i)]
+    return content 
+
+
+def check_file():
+    keys = kv.getkeys_by_prefix('')
+    if not keys:
+        content = ['Empty Diary']
+    else:
+        content = diary_lines() 
+
+    return content
+    
+
+def diary_write(data):  
+    wtime = datetime.now()
+    str_wtime = wtime.strftime("%Y-%m-%d-%H-%M-%S")
+
+    key = 'Null:'+str_wtime
+    kv.set(key, data)
+   
+def cmd_output(list_k):
+    TAG = 'Null'
+    str_bytes = "Diary History:\n"
+    str_bytes += "TAG as:%s\n" % TAG 
+    for line in list_k:
+        str_bytes += line
+        str_bytes += '\n' 
+
+    return str_bytes
+
+
+@app.get('/diary')
+def welcome():
+    history = check_file()
+    return template('welcome_diary', content = history)
+
+@app.post('/diary')
+def print_input():
+    client_input = request.forms.get('content')
+    diary_write(client_input)
+    data = diary_lines()
+    return template('welcome_diary', content=data)
+
+@app.get('/cmd')
+def login():
+    history_cmd = check_file()    
+    return cmd_output(history_cmd)
+
+@app.post('/cmd')
+def cmd_e():
+    key = request.forms.get('key')
+ #   value = request.forms.get('value')
+    print key
+
+
+application = sae.create_wsgi_app(app)
+
+
