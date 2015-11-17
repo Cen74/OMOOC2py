8630b2ea582745c6f20d09afe199805c263759c5
Cen74 <52993580@qq.com>
2015-11-18 07:20:08 +0800

    update

--- _src/om2py5w/5wex0/paas_client.py
+++ _src/om2py5w/5wex0/paas_client.py
@@ -20,11 +20,12 @@ def foo():
 	print '-'*10
 
 
-	TAG = '>>'
+	TAG = 'Null'
 
 	while True:
 		input_c = raw_input('%s>' % TAG)
 
+		print input_c[0:3]
 		if input_c in ['help', 'h', '?']:
 			print """
 			   \'q\' or \'quit\' \'bye\' for help
@@ -43,12 +44,24 @@ def foo():
 		elif input_c in ['bye', 'q' ,'quit']:
 			print "bye"
 			break
+		
 		elif input_c == 'syn':
 			history = requests.get('http://localhost:8080/diary')
 			print "The diary histroy is :", '-'*10
 			print_html(history.text)
-		elif input_c[0:2] == 'st:'
+		
+		elif input_c[0:3] == 'st:':
 			TAG = input_c[3:]
+			print TAG
+			if TAG != []:  # is not [] 不起作用
+				print 'not Null'
+				set_TAG = requests.post("http://localhost:8080/cmd/set", data = {"key":TAG})
+				print set_TAG.text
+
+			else:
+				TAG = 'Null'
+				print 'finsish'
+
 			
 
 		else:
